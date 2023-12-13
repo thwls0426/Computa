c51eeda129bac438511b6a91421baf1ce486cf2b
thwls0426 <thwls950426@gmail.com>
2023-12-13 12:51:35 +0900

    new
    시작파일

---
+++ src/main/java/com/example/demo/ComputaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputaApplication.class, args);
+	}
+
+}
